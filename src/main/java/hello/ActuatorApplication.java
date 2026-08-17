2cbd8ba5723235c0e3b6baee1e38967547b4f166
jun200 <82015977+snow1000000000000000000@users.noreply.github.com>
2026-08-17 23:33:09 +0900

    22222

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -1,13 +1,24 @@
 package hello;
 
+import hello.order.v1.OrderConfigV1;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+
+@Import(OrderConfigV1.class) // 예제를 바꿔가면서할거니까
+@SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
     }
 
+
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 }
